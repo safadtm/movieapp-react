d25969bf5d3f37e73747cd23b116dccda4a38ee6
safadtm <safadtm2004@gmail.com>
2023-12-09 08:56:07 +0530

    movie card --created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import {useEffect} from 'react'
+import { useEffect } from 'react'
 
+import './App.css';
+import SearchIcon from './search.svg'
 
-const API_URL = 'http://www.omdbapi.com?apikey=1dae8ebc' 
+const API_URL = 'http://www.omdbapi.com?apikey=1dae8ebc'
 
 const App = () => {
 
   const searchMovies = async (title) => {
-   const response = await fetch(`${API_URL}&s=${title}`);
-   const data = await response.json();
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
 
-   console.log(data.Search);
+    console.log(data.Search);
 
   }
-   
+
   useEffect(() => {
-    
-  searchMovies('Spiderman');
-    
+
+    searchMovies('Spiderman');
+
   }, []);
-  
+
 
   return (
-    <div>App</div>
+    <div className='app'>
+      <h1>MovieLand</h1>
+
+      <div className='search'>
+        <input
+          placeholder='Search for movies'
+          value="Superman"
+          onChange={() => { }}
+        />
+        <img
+          src={SearchIcon}
+          alt='search'
+          onClick={() => { }}
+        />
+      </div>
+
+      <div className='container'>
+
+      </div>
+    </div>
   );
 }
 
